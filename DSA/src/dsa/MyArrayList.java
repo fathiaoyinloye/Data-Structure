65a07d94eea7c98d601d6c208112d857ee627db1
fathiaoyinloye <fathiaoyinloye20@gmail.com>
2025-11-30 07:53:48 +0100

    ArrayList Working Fine

--- DSA/src/dsa/MyArrayList.java
+++ DSA/src/dsa/MyArrayList.java
@@ -2,12 +2,11 @@ package dsa;
 
 public class MyArrayList {
     private int count =  0;
-
-
-
+    private String[] elements;
+    private int myArrayListSize = 30;
 
     public MyArrayList() {
-
+        elements = new String[myArrayListSize];
     }
 
     public boolean isEmpty() {
@@ -15,7 +14,84 @@ public class MyArrayList {
     }
 
     public boolean add(String element) {
+        elements[count] = element;
         count++;
-    return true;
+        changeSize();
+        return true;
     }
+    public int lastElementIndex() {
+        return count - 1;
+    }
+    public boolean myRemove(String element) {
+        boolean isThere = false;
+        for(int counter = 0; counter < count; counter++){
+            if(elements[counter].equals(element)){
+                isThere = true;
+                count--;
+                break;
+
+            }
+
+        }
+        return isThere;
+    }
+    private void changeSize() {
+        if (count == myArrayListSize/2) {
+            myArrayListSize += myArrayListSize;
+            String[] newElements = new String[myArrayListSize];
+            elements = newElements;
+            for(int counter = 0; counter < myArrayListSize; counter++){
+                newElements[counter] = elements[counter];
+            }
+
+        }
+
+    }
+    public String get(int index){
+        if(count == 0) throw new IllegalArgumentException("Empty ArrayList");
+
+        return elements[index];
+    }
+    public int size(){
+        return count;
+    }
+    public void myRemove(int index){
+        for(int counter = 1; counter < count; counter++){
+          for(int check = 0; check < count; check++) {
+              if (elements[check].equals(elements[index])) {
+                  elements[check] = elements[counter];
+              }
+          }
+        }
+        elements[lastElementIndex()] = null;
+        count --;
+    }
+    public void add(int index, String element) {
+        count++;
+        for(int check = index; check < count; check++) {
+            for (int counter = check + 1; counter < count; counter++) {
+                String temp = elements[check];
+                elements[check] = element;
+                elements[counter] = temp;
+            }
+        }
+    }
+
+    public boolean contains(String element){
+        boolean result = false;
+        for(int counter = 0; counter < count; counter++) {
+            if (elements[counter].equals(element)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
 }
--- DSA/test/dsa/ArrayListTest.java
+++ DSA/test/dsa/ArrayListTest.java
@@ -1,37 +1,147 @@
 package dsa;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayListTest {
+        MyArrayList list;
+        @BeforeEach
+        void setUp(){
+            list = new MyArrayList();
+        }
 
     @Test
     public void myArrayList_isEmpty_test() {
-        MyArrayList list = new MyArrayList();
         assertTrue(list.isEmpty());
     }
     @Test
     public void addedOneElement_ListNotEmpty() {
-        MyArrayList list = new MyArrayList();
         assertTrue(list.isEmpty());
         list.add("elements");
         assertFalse(list.isEmpty());
     }
     @Test
     public void addTwoElements_ListNotEmpty() {
-        MyArrayList list = new MyArrayList();
         assertTrue(list.isEmpty());
         list.add("elements");
         list.add("second element");
         assertFalse(list.isEmpty());
     }
     @Test
-    public void addtwoelements_ListNotEmpty_lastElementAddedToEndOfList() {
-        MyArrayList list = new MyArrayList();
+    public void addTwoElements_ListNotEmpty_lastElementAddedToEndOfList() {
         list.add("elements");
         list.add("second element");
+        assertFalse(list.isEmpty());
+        assertEquals(1,list.lastElementIndex());
 
+    }
+    @Test
+    public void addOneElement_removeOneElement_listIsEmpty() {
+        assertTrue(list.isEmpty());
+        list.add("element");
+        list.myRemove("element");
+        assertTrue(list.isEmpty());
+
+    }
+
+    @Test
+    public void AddTwoElements_removeOneElement_listIsNotEmpty() {
+        assertTrue(list.isEmpty());
+        list.add("element");
+        list.add("second element");
+        assertFalse(list.isEmpty());
+        list.myRemove("second element");
+        assertFalse(list.isEmpty());
+    }
+    @Test
+    public void TestThatTwoElemenetsIsAdded_TwoRemoved_ListIsEmpty() {
+        assertTrue(list.isEmpty());
+        list.add("element");
+        list.add("second element");
+        assertFalse(list.isEmpty());
+        list.myRemove("second element");
+        list.myRemove("element");
         assertTrue(list.isEmpty());
     }
+
+   @Test
+    public void testThatFiftyElementsCanBeAdded_ListIsNotEmpty() {
+        assertTrue(list.isEmpty());
+        for(int count = 1; count <= 100; count++){
+            list.add("element" + count);
+        }
+        assertFalse(list.isEmpty());
+    }
+    @Test
+    public void TestMyRemoveDoesNotRemoveElementNotInMyArrayList() {
+        assertTrue(list.isEmpty());
+        list.add("element");
+        assertFalse(list.isEmpty());
+        assertFalse(list.myRemove("third element"));
+        assertFalse(list.myRemove("second element"));
+    }
+    @Test
+    public void testThatAnElementCaneRemovedByItIndex(){
+     assertTrue(list.isEmpty());
+     list.add("element");
+     list.add("second element");
+     list.add("third element");
+     list.myRemove(1);
+     assertEquals("third element", list.get(1));
+    }
+    @Test
+    public void testThatExceptionIsThrown_attemptToGetAnEmptyArraYList(){
+        assertThrows(IllegalArgumentException.class, () -> list.get(0));
+
+    }
+    @Test
+    public void testThatAnElementCanBeRemovedByIndex_sizeOMyListChanges(){
+     assertTrue(list.isEmpty());
+     list.add("element");
+     list.add("second element");
+     list.add("third element");
+     list.myRemove(1);
+     assertEquals("third element", list.get(1));
+     assertEquals(2, list.size());
+   }
+    @Test
+    public void testthatTheSizeOfArrayListCanBeChecked(){
+        assertTrue(list.isEmpty());
+        list.add("element");
+        list.add("second element");
+        list.add("third element");
+        assertFalse(list.isEmpty());
+        assertEquals(3, list.size());
+    }
+    @Test
+    public void   testThatElementCanBeAddedAtSpecificIndex(){
+                      assertTrue(list.isEmpty());
+        list.add("element");
+        list.add("second element");
+        list.add(1, "third element");
+        assertEquals("third element", list.get(1));
+
+    }
+   @Test
+   public void testThatElementCanBeCheckedIfItIsInTheList(){
+         assertTrue(list.isEmpty());
+         list.add("element");
+         list.add("second element");
+        assertTrue(list.contains("second element"));
+    }
+
+   @Test
+   public void testThatElementCanBeCheckedIfItIsNotInTheList(){
+         assertTrue(list.isEmpty());
+        list.add("element");
+        list.add("second element");
+        assertFalse(list.contains("third element"));
+  }
+
+
+
+
+
 }
