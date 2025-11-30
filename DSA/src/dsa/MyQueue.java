e124693a5ad8d8a10aa4ea84742e66414d46652f
fathiaoyinloye <fathiaoyinloye20@gmail.com>
2025-11-30 16:05:23 +0100

    Queue Working

---
+++ DSA/src/dsa/MyQueue.java
@@ -0,0 +1,64 @@
+package dsa;
+
+public class MyQueue {
+    int count = 0;
+    String[] elements = new String[5];
+
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+   public  boolean myOffer(String element){
+        if(count < 5) {
+            elements[count] = element;
+            count++;
+        }
+        return  count < 5;
+   }
+
+    public  boolean myAdd(String element){
+        if(count == 5) throw new IllegalArgumentException("Queue is full");
+        if(count < 5) {
+            elements[count] = element;
+            count++;
+        }
+        return  count < 5;
+    }
+
+
+
+   public String myPoll(){
+        for(int counter = 0; counter < count - 1; counter++){
+            for(int index = 1; index < count - 1; index++){
+                elements[counter] = elements[index];
+            }
+        }
+       elements[count] = null;
+       count--;
+       return elements[0];
+
+   }
+    public String remove(){
+        if (count == 0) throw new IllegalArgumentException("Queue is empty");
+        for(int counter = 0; counter < count - 1; counter++){
+            for(int index = 1; index < count - 1; index++){
+                elements[counter] = elements[index];
+            }
+        }
+        elements[count] = null;
+        count--;
+        return elements[0];
+
+    }
+    public String element(){
+        if(count == 0) throw new IllegalArgumentException("Queue is empty");
+        return elements[0];
+    }
+    public String peek(){
+        if(count == 0) return null;
+        return elements[0];
+    }
+
+
+}
---
+++ DSA/test/dsa/QueueTest.java
@@ -0,0 +1,149 @@
+package dsa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QueueTest {
+    MyQueue queue;
+    @BeforeEach
+    void setUp() {
+        queue = new MyQueue();
+    }
+    @Test
+    public void testThatQueueIsEmpty() {
+        assertTrue(queue.isEmpty());
+
+
+    }
+    @Test
+    public void testThatElementXAdded_stackNotEmpty() {
+        assertTrue(queue.isEmpty());
+        assertTrue(queue.myOffer("element"));
+        assertFalse(queue.isEmpty());
+
+
+    }
+
+    @Test
+    public void addElementXYToMyQueue_QueueIsNotEmpty() {
+        assertTrue(queue.isEmpty());
+        assertTrue(queue.myOffer("element"));
+        assertTrue(queue.myOffer("second element"));
+        assertFalse(queue.isEmpty());
+
+    }
+
+    @Test
+    public void addElementX_pollElementX_queueIsEmpty() {
+        MyQueue queue = new MyQueue();
+        assertTrue(queue.isEmpty());
+        assertTrue(queue.myOffer("element"));
+        queue.myPoll();
+        assertTrue(queue.isEmpty());
+    }
+    @Test
+    public void addElementXY_pollElementX_queueIsNotEmpty() {
+        assertTrue(queue.isEmpty());
+        queue.myOffer("X");
+        queue.myOffer("Y");
+        queue.myPoll();
+        assertFalse(queue.isEmpty());
+    }
+    @Test
+    public void addElementXY_pollElementX_queueIsNotEmpty_removedFirstElement() {
+        assertTrue(queue.isEmpty());
+        queue.myOffer("X");
+        queue.myOffer("Y");
+        assertFalse(queue.isEmpty());
+        assertEquals("X", queue.myPoll());
+    }
+    @Test
+    public void addElementXYZ_offerElementXY_queueIsNotEmpty_pollReturnsFirstElement() {
+        assertTrue(queue.isEmpty());
+        queue.myOffer("X");
+        queue.myOffer("Y");
+        queue.myOffer("Z");
+        queue.myPoll();
+        assertEquals("Y", queue.myPoll());
+    }
+    @Test
+    public void queueIsEmpty_pollElementXFromMyQueue_ReturnsNull() {
+        assertTrue(queue.isEmpty());
+        assertNull(queue.myPoll());
+    }
+    @Test
+    public void testThatMyOfferReturnsFalseWhenItsFull(){
+        assertTrue(queue.isEmpty());
+        queue.myOffer("W");
+        queue.myOffer("X");
+        queue.myOffer("Y");
+        queue.myOffer("Z");
+        queue.myOffer("W");
+        assertFalse(queue.myOffer("X"));
+
+    }
+    @Test
+    public void addElementX_removeElementX_queueIsEmpty() {
+        assertTrue(queue.isEmpty());
+        queue.myOffer("X");
+        assertFalse(queue.isEmpty());
+        assertEquals("X", queue.remove());
+        assertTrue(queue.isEmpty());
+
+    }
+    @Test
+    public void addElementXY_removeElementX_queueIsNotEmpty_removedFirstElement() {
+        assertTrue(queue.isEmpty());
+        queue.myAdd("X");
+        queue.myAdd("Y");
+        assertFalse(queue.isEmpty());
+        assertEquals("X", queue.remove());
+    }
+
+   @Test
+    public void testThatRemovedThrowsExceptionWhenQueueIsEmpty(){
+        assertTrue(queue.isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> queue.remove());
+    }
+
+    @Test
+    public void testThatmyAddThrowsExceptionWhenQueueIsFilled(){
+        assertTrue(queue.isEmpty());
+        queue.myAdd("X");
+        queue.myAdd("Y");
+        queue.myAdd("Z");
+        queue.myAdd("W");
+        queue.myAdd("X");
+        assertThrows(IllegalArgumentException.class, () -> queue.myAdd("P"));
+    }
+    @Test
+    public void testThatFirstElementCanBeChecked(){
+        assertTrue(queue.isEmpty());
+        queue.myOffer("X");
+        queue.myOffer("Y");
+        queue.myOffer("Z");
+        assertEquals("X", queue.element());
+    }
+    @Test
+    public void testThatElementThrowsExceptionWhenQueueIsEmpty(){
+        assertTrue(queue.isEmpty());
+        assertThrows(IllegalArgumentException.class, () -> queue.element());
+    }
+
+    @Test
+    public void testThatFirstElementCanBePeeked(){
+        assertTrue(queue.isEmpty());
+        queue.myOffer("X");
+        queue.myOffer("Y");
+        queue.myOffer("Z");
+        assertEquals("X", queue.peek());
+    }
+    @Test
+    public void testThatPeekReturnNullWhenQueueIsEmpty(){
+        assertTrue(queue.isEmpty());
+        assertEquals(null,queue.peek());
+    }
+
+}
